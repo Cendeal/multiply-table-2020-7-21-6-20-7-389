418deb1fa6ad1cd5af9a8b79677e48212f82e805
Cendeal <1798062051@qq.com>
2020-07-22 00:53:51 +0800

    finish and push code

---
+++ contextMapAndPDCD/PDCD.md
@@ -0,0 +1,11 @@
+#### Draw Context map:
+- P: 15 minutes
+- D: 16 minutes
+- C: some classmate ask me one question, i try to answer it and spend some time.
+- D: I think I need to do it  more focused
+#### Write Code:
+- P: 30 minutes
+- D: 34 minutes
+- C: one line logic is not clear
+- D: I think I need to analyze it more Accurate
+
--- main.js
+++ main.js
@@ -1,7 +1,38 @@
 function createMultiplyTable(start, end) {
-  return null;
+    if (!isValid(start, end)) {
+        return null;
+    }
+    return generateMultiplyAll(start, end)
+}
+
+function isValid(start, end) {
+    return start < end && start >= 1 && end <= 1000
+}
+
+function generateMultiplyAll(start, end) {
+    let table = ''
+    for (let i = start; i <= end; i++) {
+        table += generateOneLineMultiplication(start, i)
+        if (i !== end) {
+            table += '\n'
+        }
+    }
+    return table
+}
+
+function generateOneLineMultiplication(lineStart, lineEnd) {
+    let expressions = []
+    for (let i = lineStart; i <= lineEnd; i++) {
+        expressions.push(generateOneExpression(i, lineEnd))
+    }
+    return expressions.join('\t')
+}
+
+function generateOneExpression(multiplicand, multiplier) {
+    return `${multiplicand}*${multiplier}=${multiplier * multiplicand}`
 }
 
 module.exports = {
-  createMultiplyTable,
+    createMultiplyTable,
 };
+
